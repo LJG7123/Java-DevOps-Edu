feb3be800759e316b0c311b717f69f07f95873d3
LJG7123 <ljg7123@naver.com>
2026-03-31 17:48:33 +0900

    0331 commit

---
+++ Web_Basic/VS_Work/step03_JavaScript/js/basic.js
@@ -0,0 +1,19 @@
+// 여기는 자바스크립트 영역입니다.
+/* 여러줄 또는 부분 주석입니다. */
+
+// 콘솔에 출력
+console.log("어디에 출력되나요?");
+
+// 브라우저에 출력
+document.write("난 어디에 나오니?");
+
+// 함수 작성
+function test(th) {
+  // alert("되나?");
+  // h2에 css 적용
+  // element를 찾는 방법 - id로 찾기
+  // document.getElementById("h2").style.backgroundColor = "red";
+  // document.getElementById("h2").style.color = "white";
+  th.style.backgroundColor = "red";
+  th.style.color = "white";
+}
